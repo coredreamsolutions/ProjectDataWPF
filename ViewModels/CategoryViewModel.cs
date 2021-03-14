eee7daf8651ba272eb723cfa82c530f38e07e83f
Dan Maresca <admin@coredreamsolutions.com>
2021-03-14 19:05:49 -0400

    More MVVM

--- ViewModels/CategoryViewModel.cs
+++ ViewModels/CategoryViewModel.cs
@@ -1,29 +1,40 @@
 ﻿using Dapper;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using ProjectDataWPF.Models;
 using ProjectDataWPF.Services;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ProjectDataWPF.ViewModels
 {
-    public class CategoryViewModel
+    public class CategoryViewModel : ObservableObject
     {
+        public CategoryViewModel()
+        {
+            GetCategoriesCommand = new RelayCommand(GetCategories);
+        }
+
+        public ICommand GetCategoriesCommand { get; }
+
         public ObservableCollection<Category> Categories { get; set; }
 
         public void GetCategories()
         {
+
             using IDbConnection cnn = Database.GetConnection();
 
             var result = cnn.Query<Category>(
                 $"SELECT * FROM CategoryDefinitions WHERE CategoryNumber > 0 ORDER BY CategoryName ASC;").ToList();
 
             Categories = new ObservableCollection<Category>(result);
-
         }
-
-       
-
     }
+
+
 }
