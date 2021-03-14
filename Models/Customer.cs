66751a9cf583dc1e2f561683ec5b292c7b8d15ee
Dan Maresca <admin@coredreamsolutions.com>
2021-03-14 17:08:02 -0400

    CLEANED UP MVVM

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,11 +12,7 @@ namespace ProjectDataWPF.Models
     public class Customer : INotifyPropertyChanged
     {
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+   
 
         public long CustomerNumber { get; set; }
         public string CustomerName { get; set; }
@@ -24,33 +20,5 @@ namespace ProjectDataWPF.Models
         public string CustomerAddress1 { get; set; }
         public string CustomerAddress2 { get; set; }
         public long CustomerMobileNumber { get; set; }
-
-        private static Timer aTimer;
-
-        public Customer()
-        {
-            CustomerNumber = 1;
-            CustomerName = "James Holden";
-            CustomerAge = 32;
-            CustomerAddress1 = "123 Penny Lane";
-            CustomerAddress2 = "Apt #3C";
-            CustomerMobileNumber = 0001112222;
-
-            SetTimer();
-        }
-
-        public void SetTimer()
-        {
-            aTimer = new Timer(1000);
-            aTimer.Elapsed += ATimer_Elapsed;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
-        private void ATimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            CustomerNumber++;
-            OnPropertyChanged("CustomerNumber");
-        }
     }
 }
---
+++ ViewModels/CustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDataWPF.ViewModels
+{
+    public class CustomerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+
+    }
+}
